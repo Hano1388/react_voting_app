620e903dd04d90c3846565d446d9c1df35feef53
Hano <hinsul3113@gmail.com>
2018-04-30 02:47:21 -0700

    created product component and a formatted template to list each product

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,39 @@
+
 class ProductList extends React.Component {
     render() {
         return (
             <div className='single-product'>
-                Hello, friend! I am a basic React component.
+                <Product />
             </div>
         );
+    }
+}
+
+class Product extends React.Component {
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-lg-4'>
+                    <img className='img-fluid product-image' 
+                    src='images/products/image-aqua.png' />
+                </div>
+                <div className='col-lg-8 details'>
+                    <div className='row'>
+                        <div className='description'>
+                            <a href='#'>Fort Knight</a>
+                            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                        </div>
+                    </div>
+                    <div className='row extra'>
+                        <span>Submitted by:</span>
+                        <img
+                        className='img-fluid author-image'
+                        src='images/avatars/daniel.jpg'
+                        />
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
